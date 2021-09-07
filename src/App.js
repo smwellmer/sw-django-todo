ea68d55d4d93dafecb1ecdbe604e9e345c0a8ba6
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-09-07 16:09:34 -0400

    end of day tuesday

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import AllPosts from "./pages/AllPosts";  
+import SinglePost from "./pages/SinglePost";
+import Form from "./pages/Form";
+
+import React, {useState, useEffect} from 'react'
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
+  /////////////////////
+  // Style Objects
+  /////////////////////
+  const h1 = {
+    textAlign: 'center',
+    margin: '10px'
+  }
+  /////////////////////////
+  // State & Other Variables
+  /////////////////////////
+
+  // API URL
+  const url = "https://am-628-django-todos.herokuapp.com/todos/"
+
+  // State to Hold List of Todos
+  const [posts, setPosts] = useState([])
+
+  //////////////////////////
+  // Functions
+  //////////////////////////
+
+  const getTodos = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setPosts(data)
+  }
+  //////////////////////////
+  // useEffects
+  //////////////////////////
+
+  useEffect(() => {getTodos()}, [])
+
+  //////////////////////////////
+  // Returned JSX
+  //////////////////////////
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={h1}> My To-Do List </h1>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => <AllPosts {...routerProps} posts={posts} />}
+        />
+        <Route
+          path="/post/:id"
+          render={(routerProps) => <SinglePost {...routerProps} posts={posts} />}
+        />
+        <Route
+          path="/new"
+          render={(routerProps) => <Form {...routerProps}/>}
+        />
+        <Route
+          path="/edit"
+          render={(routerProps) => <Form {...routerProps}/>}
+        />
+      </Switch>
     </div>
   );
 }
+
+
+
 
 export default App;
